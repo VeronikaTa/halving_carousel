0a08bf1264f79362a088975a9940444d051f472e
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-10-04 21:40:17 +0300

    edited CarouselRun and DecrementingCarousel

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,71 @@
 package com.epam.rd.autotasks;
 
+import java.util.LinkedList;
+
 public class CarouselRun {
+    LinkedList<Integer> carousel = new LinkedList<>();
+    int index = 0;
+    private String getClass;
+
+    CarouselRun(LinkedList<Integer> carousel, String getClass){
+        for(Integer element : carousel){
+            this.carousel.add(element);
+        }
+        this.getClass = getClass;
+    }
 
     public int next() {
-       throw new UnsupportedOperationException();
+        if(isFinished()){
+            return -1;
+        }
+        int current = 0;
+        boolean nextElementFound = false;
+        while(!nextElementFound){
+            if(this.carousel.get(this.index) > 0){
+                current = this.carousel.get(this.index);
+                this.carousel.set(this.index, this.changeElement(current));
+                changeIndex();
+                nextElementFound = true;
+            }
+            else{
+                changeIndex();
+            }
+        }
+        return current;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        boolean finished = false;
+        for(int value : this.carousel){
+            if(value == 0){
+                finished = true;
+            }
+            else{
+                finished = false;
+                break;
+            }
+        }
+        return finished;
     }
 
+    protected void changeIndex(){
+        if(index != (this.carousel.size() - 1)){
+            this.index ++;
+        }
+        else{
+            this.index = 0;
+        }
+    }
+
+    private int changeElement(int current){
+        if(this.getClass.contains("DecrementingCarousel")){
+            return current - 1;
+        }
+        else{
+            if(this.getClass.contains("HalvingCarousel")){
+                return current / 2;
+            }
+        }
+        return 0;
+    }
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,39 @@
 package com.epam.rd.autotasks;
 
-public class DecrementingCarousel {
-    public DecrementingCarousel(int capacity) {
+import java.util.LinkedList;
 
+public class DecrementingCarousel {
+    LinkedList<Integer> carousel = new LinkedList<>();
+    int maximumCapacity;
+    boolean runCreated;
+
+    public DecrementingCarousel(int capacity) {
+        maximumCapacity = capacity;
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if(this.carousel.size() < this.maximumCapacity && element > 0 && !(this.runCreated)) {
+            this.carousel.add(element);
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 
     public CarouselRun run(){
-       throw new UnsupportedOperationException();
+        if(!this.runCreated){
+            this.runCreated = true;
+            if(this.carousel.size() == 0){
+                for(int i = 0; i < this.maximumCapacity; i++){
+                    this.carousel.add(0);
+                }
+            }
+            return new CarouselRun(this.carousel, String.valueOf(this.getClass()));
+        }
+        else{
+            return null;
+        }
     }
 }
